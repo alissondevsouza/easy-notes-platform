94d38691b0634728799882c8e563a1ed58725fa0
Alisson Rhuan de Souza <alisson.rhuan@crearesistemas.com.br>
2024-03-27 15:52:30 -0300

    Feat: add toggle to select theme mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { ModeToggle } from "./_components/mode-toggle"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
+      <ModeToggle />
       <h1> Home </h1>
     </main>
   );
